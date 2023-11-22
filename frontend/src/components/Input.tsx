1567ca61d15cc76657c8b01a6cdc47289b5061fc
forsrobin <forsrobin@gmail.com>
2023-11-22 14:03:42 +0100

    Changes

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -2,7 +2,7 @@ import { ChangeEvent, FC } from 'react'
 
 interface Props {
   label: string
-  type: 'text' | 'password' | 'email'
+  type: 'text' | 'password' | 'email' | 'number'
   placeholder: string
   onChange: (e: ChangeEvent<HTMLInputElement>) => void
   value: string | number
@@ -13,6 +13,8 @@ export const Input: FC<Props> = ({ type, placeholder, label, value, onChange }) 
     <label htmlFor={label} className='flex relative flex-col'>
       <span className=' text-xs -top-1/2 translate-y-1/2 mx-2 px-1 w-fit bg-white'>{label}</span>
       <input
+        min={0}
+        minLength={0}
         onChange={onChange}
         className='border-[1px] rounded-md px-3 py-3 border-gray-300 text-sm'
         required
